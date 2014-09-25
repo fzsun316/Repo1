b6dddcc36177f18ca277034369d818a8c4ae275c
Fangzhou Sun <fzsun316@gmail.com>
2014-09-25 15:15:31 -0500

    init

---
+++ Event.java
@@ -0,0 +1,22 @@
+package edu.vanderbilt.vandysports;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Event  implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+    public final Team mTeam;
+    public final Date date;
+    public final String event;
+    public final String location;
+    public final String time;
+
+    public Event(Team team, Date date, String event, String location, String time) {
+        mTeam = team;
+        this.date = date;
+        this.event = event;
+        this.location = location;
+        this.time = time;
+    }
+}
